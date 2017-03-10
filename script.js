4193b08bc279fc12032c1f0736eab3e11e5d4d6b
Anna Kaehr <aikaehr13@gmail.com>
2017-03-10 03:10:55 +0000

    Like buttons

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 $(document).ready(function(){
-    //Write all your jQuery code here
-        $("button").click(function() {
-      $("#like").html("You like this project!");
+    $("button1").click(function() {
+      $("#like1").html("You like this garden!");
     });
-    
-    
-    
+    $("button2").click(function() {
+      $("#like2").html("You like this garden!");
+    });
+    $("button3").click(function() {
+      $("#like3").html("You like this garden!");
+    });
+    $("button4").click(function() {
+      $("#like4").html("You like this garden!");
+    });
+    $("button5").click(function() {
+      $("#like5").html("You like this garden!");
+    });
+    $("button6").click(function() {
+      $("#like6").html("You like this garden!");
+    });
+    $("button7").click(function() {
+      $("#like7").html("You like this garden!");
+    });
+    $("button8").click(function() {
+      $("#like8").html("You like this garden!");
+    });
+    $("button9").click(function() {
+      $("#like9").html("You like this garden!");
+    });
     
 });
